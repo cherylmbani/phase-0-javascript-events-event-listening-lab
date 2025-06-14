85ecd09219cc1030edec7a9940521459cde4842e
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-14 22:41:57 +0300

    final commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+// function addingEventListener() {
+    //alert(" I was clicked");
+
+//}
+//const input = document.getElementById("button");
+//input.addEventListener("click", addingEventListener);
+
+//Most important point is the code creating an event listener should be inside the function
+function addingEventListener(){
+    const input = document.getElementById("button");
+    input.addEventListener("click", function(){
+        alert("I was clicked");
+    });   
 }
+addingEventListener();
